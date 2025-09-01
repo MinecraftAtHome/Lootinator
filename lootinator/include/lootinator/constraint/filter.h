8fe1cf4bee20efadc68d7cc07d905a10de7830d1
Kludwisz <kludwisz@gmail.com>
2025-09-01 15:46:48 +0200

    start cleaning up pool filtering

--- lootinator/include/lootinator/constraint/filter.h
+++ lootinator/include/lootinator/constraint/filter.h
@@ -4,31 +4,32 @@
 #include "lootinator/constraint/constraint.h"
 
 namespace loot {
-    enum PoolMatchType {
-        NOT_FOUND,
-        SINGLE_POOL,
-        MULTI_POOL
-    };
-    enum PoolFilterType {
-        NOT_FOUND,
-        SINGLE_POOL,
-        MULTI_POOL
+    static const int NO_POOL_MATCH = -1;
+    static const int MULTI_POOL_MATCH = -2;
+
+    enum ReversalType {
+        ITEM_ONLY,
+        ITEM_AND_ATTRIBUTE,
+        ITEM_AND_ATTRIBUTE_AND_LEVEL
     };
 
+    /*
+    The following structure describes the actual data the kernel will use for the initial filter. Unlike constraints,
+    pool filters never define ranges of possible values, which includes both item counts and attributes.
+    The item's properties will all be directly mapped to code filtering them.
+    */
     struct PoolFilter {
-        int pool_idx;
-        int item_idx;
-        int item_count;
-        std::vector<ItemAttribute> attributes;
-        float filter_score;
+        ReversalType reversal_type; // scope of reversal
+        int pool_idx; // for which pool the filter is defined
+        int entry_idx; // which entry the filter targets
+        int entry_count; // how many instances of the entry are targetted
+        ItemAttribute attribute; // required attributes of the entry
+        float filter_score = 0.0f; // heuristic performance estimate, bigger = faster
 
-        PoolMatchType find_matching_loot_pool(const LootTable& loot_table);
+        PoolFilter(ReversalType type, int pool_idx, int entry_idx, int entry_count, ItemAttribute attribute);
         void compute_filter_score(const LootTable& loot_table);
 
     private:
-        bool entry_attributes_match(const nlohmann::json& entry) const;
-        bool entry_item_matches(const LootTable& loot_table, const nlohmann::json& entry) const;
-
         float compute_forward_filter_score(const LootTable& loot_table, const float item_rarity) const;
         float compute_backward_filter_penalty(const LootTable& loot_table) const;
     };
@@ -37,11 +38,17 @@ namespace loot {
     // global constraints (constraints depending on more than 1 loot pool).
     struct LootTableConstraintList {
         LootTable& loot_table;
-        std::vector<loot::PoolFilter> per_pool_constraints;
+        std::vector<loot::PoolFilter> available_filters;
         std::vector<loot::Constraint> global_constraints;
 
         LootTableConstraintList(LootTable& loot_table);
         bool initialize_constraints(const std::vector<loot::Constraint>& constraints);
+
+    private:
+        void add_possible_filters(const std::vector<loot::Constraint>& constraints, const Constraint& main_constraint);
+        int find_matching_loot_pool(const Constraint& constr) const;
+        bool entry_attributes_match(const Constraint& constr, const nlohmann::json& entry) const;
+        bool entry_item_matches(const Constraint& constr, const nlohmann::json& entry) const;
     };
 };
 
--- lootinator/src/constraint/filter.cpp
+++ lootinator/src/constraint/filter.cpp
@@ -3,25 +3,24 @@
 namespace loot {
     // iterates over the entries for each loot pool and find one that matches
     // the stored item index and the stored attributes
-    PoolMatchType PoolFilter::find_matching_loot_pool(const LootTable &loot_table) {
-        PoolMatchType pool_match = PoolMatchType::NOT_FOUND;
-        int idx = 0;
+    int LootTableConstraintList::find_matching_loot_pool(const Constraint& constr) const {
+        int pool_match = loot::NO_POOL_MATCH;
+        int pool_idx = 0;
 
         const auto& pools = loot_table.data["pools"];
         for (const auto& loot_pool : pools) {
             const auto& entries = loot_table.data["entries"];
             for (const auto& entry : entries) {
-                if (entry_item_matches(loot_table, entry) && entry_attributes_match(entry)) {
-                    if (pool_match != PoolMatchType::NOT_FOUND) {
-                        pool_match = PoolMatchType::MULTI_POOL;
+                if (entry_item_matches(constr, entry) && entry_attributes_match(constr, entry)) {
+                    if (pool_match != loot::NO_POOL_MATCH) {
+                        pool_match = loot::MULTI_POOL_MATCH;
                     }
                     else {
-                        pool_match = PoolMatchType::SINGLE_POOL;
-                        pool_idx = idx;
+                        pool_match = pool_idx;
                     }
                 }
             }
-            idx++;
+            pool_idx++;
         }
 
         return pool_match;
@@ -29,20 +28,12 @@ namespace loot {
 
     void PoolFilter::compute_filter_score(const LootTable &loot_table) {
         // calculate item weight reduction
-        int w = 0;
-        for (int i : loot_table.precomputed_loot[pool_idx]) {
-            w += (item == i ? 1 : 0);
-        }
+        nlohmann::json entry = loot_table.data["pools"][pool_idx]["entries"][entry_idx];
+        int w = entry.contains("weight") ? entry["weight"] : 1;
         assert(w != 0);
-
         float weight_score = static_cast<float>(loot_table.total_weights[pool_idx]) / w;
-        float attribute_bonus = attributes.empty() ? 1.0f : 10.0f; // FIXME magic number, also probably requires tuning
-        float item_rarity = weight_score * attribute_bonus;
 
-        float forward_score = compute_forward_filter_score(loot_table, item_rarity);
-        float backward_penalty = compute_backward_filter_penalty(loot_table);
-
-        filter_score = item_rarity * forward_score * backward_penalty;
+        // calculate other stuff
     }
 
     float PoolFilter::compute_forward_filter_score(const LootTable &loot_table, const float item_rarity) const {
@@ -59,15 +50,15 @@ namespace loot {
         return 1.0f;
     }
 
-    bool PoolFilter::entry_item_matches(const LootTable &loot_table, const nlohmann::json& entry) const {
+    bool LootTableConstraintList::entry_item_matches(const Constraint& constr, const nlohmann::json& entry) const {
         if (entry["type"] != "minecraft:item") {
             return false;
         }
         int item_idx = loot_table.find_item_name(entry["name"]);
-        return item_idx == item;
+        return item_idx == constr.item;
     }
 
-    bool PoolFilter::entry_attributes_match(const nlohmann::json& entry) const {
+    bool LootTableConstraintList::entry_attributes_match(const Constraint& constr, const nlohmann::json& entry) const {
         // FIXME for now this is sufficient but if user wanted to filter e.g. bastion crossbows
         // they would be classified as multi-pool and thus couldn't be used as a pool constraint
         return true; 
@@ -78,28 +69,31 @@ namespace loot {
     LootTableConstraintList::LootTableConstraintList(LootTable &loot_table) : loot_table(loot_table) {}
     
     bool LootTableConstraintList::initialize_constraints(const std::vector<loot::Constraint> &constraints) {
-        for (const auto& con : constraints) {
-            loot::PoolFilter pool_con(con);
-            PoolMatchType match_result = pool_con.find_matching_loot_pool(loot_table);
-            if (match_result == PoolMatchType::NOT_FOUND) {
+        for (const auto& constr : constraints) {
+            int pool_idx = find_matching_loot_pool(constr);
+            if (pool_idx == loot::NO_POOL_MATCH) {
                 return false; // illegal constraint specified
             }
-            else if (match_result == PoolMatchType::SINGLE_POOL) {
-                pool_con.compute_filter_score(loot_table);
-                per_pool_constraints.push_back(pool_con);
+            else if (pool_idx == loot::MULTI_POOL_MATCH) {
+                global_constraints.push_back(constr);
             }
             else {
-                global_constraints.push_back(con);
+                add_possible_filters(constraints, constr);
             }
         }
 
         // sort the per-pool constraint vector in descending order using the computed heuristic score
-        std::sort(per_pool_constraints.begin(), per_pool_constraints.end(), 
+        std::sort(available_filters.begin(), available_filters.end(), 
             [](const PoolFilter& a, const PoolFilter& b) {
                 return a.filter_score > b.filter_score;
             }
         );
         
         return true;
+    }
+
+    void add_possible_filters(const std::vector<loot::Constraint>& constraints, const Constraint& main_constraint) {
+        // TODO
+        // this will add unique filters to the possible filter list
     }
 }

815f70f1fa533ce0dd177dd88b00edb95d671b30
Kludwisz <kludwisz@gmail.com>
2025-10-10 19:21:40 +0200

    fix bimap implementation

--- lootinator/include/lootinator/utility/enum_bimap.hpp
+++ lootinator/include/lootinator/utility/enum_bimap.hpp
@@ -22,7 +22,7 @@ namespace loot {
     template <typename T>
     inline EnumToStringBimap<T>::EnumToStringBimap(std::initializer_list<std::pair<T, std::string>> il)
     {
-        for (std::pair<T, std::string>& elem : il) {
+        for (const auto& elem : il) {
             enum_to_string.emplace(elem.first, elem.second);
             string_to_enum.emplace(elem.second, elem.first);
         }
@@ -31,13 +31,13 @@ namespace loot {
     template <typename T>
     inline bool EnumToStringBimap<T>::contains_string(const std::string &str) const
     {
-        return enum_to_string.find(str);
+        return string_to_enum.find(str) != string_to_enum.end();
     }
 
     template <typename T>
     inline bool EnumToStringBimap<T>::contains_enum(const T value) const
     {
-        return string_to_enum.find(value);
+        return enum_to_string.find(value) != enum_to_string.end();
     }
 
     template <typename T>

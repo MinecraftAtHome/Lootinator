dd6bb05eea830670f090bf1a5793e6411e79e0a4
Kludwisz <kludwisz@gmail.com>
2025-11-02 22:16:14 +0100

    define function shared memory gen interface

--- lootinator/include/lootinator/mc/loot_functions.hpp
+++ lootinator/include/lootinator/mc/loot_functions.hpp
@@ -3,8 +3,8 @@
 
 #include "lootinator/mc/minecraft.hpp"
 
-namespace loot {
-
+namespace mc {
+    std::vector<int> get_shared_memory_for_function(const loot::LootTable &loot_table, const nlohmann::json &entry, const int function_id);
 }
 
 #endif
--- lootinator/src/mc/loot_functions.cpp
+++ lootinator/src/mc/loot_functions.cpp
@@ -1,4 +1,15 @@
 #include "lootinator/mc/loot_functions.hpp"
+#include "lootinator/loot_table.h"
+
+namespace mc {
+    std::vector<int> get_shared_memory_for_function(const loot::LootTable &loot_table, const nlohmann::json &entry, const int function_id)
+    {
+    }
 
 
-//...
+
+
+    std::vector<mc::Enchantment> create_enchant_randomly_vector() {
+
+    }
+}

ab837c37cb06a78b14a1c290011b44bcfe09d602
Kludwisz <kludwisz@gmail.com>
2026-02-01 22:43:22 +0100

    add bruteforce kernel boilerplate

--- lootinator/include/lootinator/kgen/bruteforce_kernel.hpp
+++ lootinator/include/lootinator/kgen/bruteforce_kernel.hpp
@@ -7,9 +7,11 @@ namespace kgen {
     class BruteforceKernel : public Kernel {
     public:
         static void gen_kernels(data::LootTableRoot& root_node, std::vector<ConfiguredKernel>& out);
+        BruteforceKernel(data::LootTableRoot &root_node);
 
     protected:
         virtual ConfiguredKernel generate() const override;
+        std::string to_string() const;
         std::string generate_forward_filter() const;
     };
 }
--- lootinator/src/kgen/bruteforce_kernel.cpp
+++ lootinator/src/kgen/bruteforce_kernel.cpp
@@ -1,25 +1,38 @@
 #include "lootinator/kgen/bruteforce_kernel.hpp"
+#include "stdlib.h"
+
 
 namespace kgen {
-    void kgen::BruteforceKernel::gen_kernels(data::LootTableRoot& root_node, std::vector<ConfiguredKernel>& out)
-    {
-        BruteforceKernel bk;
+    void BruteforceKernel::gen_kernels(data::LootTableRoot& root_node, std::vector<ConfiguredKernel>& out) {
+        BruteforceKernel bk(root_node);
         out.push_back(bk.generate());
     }
 
-    ConfiguredKernel kgen::BruteforceKernel::generate() const
-    {
-        return ConfiguredKernel(
-            //name
-            //code
+    BruteforceKernel::BruteforceKernel(data::LootTableRoot &root_node) : Kernel(root_node) {}
 
-            //threadstotal
-            //
-        );
+    ConfiguredKernel BruteforceKernel::generate() const
+    {
+        uint32_t idx = (rand() << 15) | rand();
+
+        return ConfiguredKernel{
+            "kernel_" + std::to_string(idx),
+            to_string(),
+            UINT64_C(1) << 48,
+            UINT64_C(1) << 32,
+            256,
+            combined_shared_memory
+        };
     }
 
-    std::string kgen::BruteforceKernel::generate_forward_filter() const
-    {
-        return std::string();
+    std::string BruteforceKernel::to_string() const {
+        return 
+R"(
+This is where the next session should start!
+Put the source code for a bruteforce kernel here, make use of generate_forward_filter()
+)";
+    }
+
+    std::string BruteforceKernel::generate_forward_filter() const {
+        return std::string(); // TODO
     }
 }

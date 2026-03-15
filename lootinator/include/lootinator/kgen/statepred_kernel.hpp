2bc5acbe9c29ac6d351f3aa5d1127cc628c38de4
Kludwisz <kludwisz@gmail.com>
2026-03-15 23:10:25 +0100

    more random changes

--- lootinator/include/lootinator/kgen/statepred_kernel.hpp
+++ lootinator/include/lootinator/kgen/statepred_kernel.hpp
@@ -16,11 +16,41 @@ namespace kgen {
 		StatepredKernel(data::LootTableRoot& root_node, data::LootEntry* target_entry,
 			loot::Constraint& target_constraint, const kgen::KernelGenConfig& kgen_config);
 
+
+		struct SharedEntryData {
+			uint32_t entry_data;
+			uint64_t enchantment_mask;
+			uint32_t enchantment_mask_2;
+			uint32_t item;
+			uint32_t item_idx;
+			uint32_t min_count;
+			uint32_t max_count;
+
+			SharedEntryData(int pool_off, int bpe, data::LootEntry* entry, std::vector<uint32_t>& shared_mem) {
+				item = entry->next_int_range.min;
+				entry_data = shared_mem[pool_off + item * bpe];
+				enchantment_mask =  shared_mem[pool_off + item * bpe + 1];
+				enchantment_mask_2 = shared_mem[pool_off + item * bpe + 2];
+				if (bpe >= 3) {
+					enchantment_mask |= (static_cast<uint64_t>(enchantment_mask_2) << 32);
+				}
+				item_idx = entry_data >> 24;
+				min_count = (entry_data >> 18) & 0x3f; 
+				max_count = (entry_data >> 12) & 0x3f; 
+			}
+		};
+
+		void emit_function_set_count(std::ostream& out, const SharedEntryData& data);
+		void emit_entry_function_enchant_randomly(std::ostream& out, const SharedEntryData& data);
+		void emit_entry_function_enchant_with_levels(std::ostream& out, const SharedEntryData& data);
+		void emit_entry_function_apply_damage(std::ostream& out, const SharedEntryData& data);
+
 		virtual ConfiguredKernel generate() override;
 
 	  protected:
 		std::string to_string();
 
+		void emit_state_prediction_entry_handler(std::ostream& out);
 		virtual void generate_statepred_filter(std::ostream& out);
 	};
 } // namespace kgen
--- lootinator/src/kgen/statepred_kernel.cpp
+++ lootinator/src/kgen/statepred_kernel.cpp
@@ -73,17 +73,79 @@ namespace kgen {
 		return std::string();
 	}
 
+	// ------------------------------------------------
+
+	
+
+	void StatepredKernel::emit_function_set_count(std::ostream& out, const SharedEntryData& data) {
+		out << "i32 item_count = nextIntBounded(&loot_seed, " << data.min_count << ", " << data.max_count << ");";
+	}
+
+	void StatepredKernel::emit_entry_function_enchant_randomly(std::ostream& out, const SharedEntryData& data) {
+		int bpe = this->kgen_config.bytes_per_entry;
+
+		if (!(data.enchantment_mask & 1)) {
+			
+			out << R"(
+			u32 enchantment_count = entry_data & 0xff; // [8b]
+			i32 enchant_id = enchantment_count != 0 ? nextInt(&loot_seed, enchantment_count) : 64;
+
+			bool r = ((enchantment_mask >> 1) & (1 << enchant_id));
+			u64 m = !r - 1;
+			loot_seed = (loot_seed * (1|(25214903917&m)) + (11&m)) & MASK_48;)";
+		}
+	}
+
+	void StatepredKernel::emit_entry_function_enchant_with_levels(std::ostream& out, const SharedEntryData& data) {
+		out << "if (enchantment_mask & 1) { enchant_with_levels_function(&loot_seed, "
+			   "&(data[enchantment_mask >> 1])); }";
+	}
+
+	void StatepredKernel::emit_entry_function_apply_damage(std::ostream& out, const SharedEntryData& data) {
+		std::string apply_damage_bitmask = create_apply_damage_item_mask(pool);
+		std::string one = pool->children.size() > 32 ? "((u64)1)" : "((u32)1)";
+
+
+		out << "if (" << apply_damage_bitmask << " & (" << one << " << " << item_idx << R"()) {
+			loot_seed = (loot_seed * 25214903917 + 11) & MASK_48;
+		})";
+	}
+
+	void StatepredKernel::emit_state_prediction_entry_handler(std::ostream& out) {
+		for (auto& child : entry->children) {
+			CAST_CHILD(func, data::LootFunctionData, child); // no nullptr
+
+			switch (func->type) {
+				case data::ENCHANT_WITH_LEVELS: {
+					emit_entry_function_enchant_with_levels(out);
+					break;
+				}
+				case data::ENCHANT_RANDOMLY: {
+					emit_entry_function_enchant_randomly(out);
+					break;
+				}
+				case data::APPLY_DAMAGE: {
+					emit_entry_function_apply_damage(out, pool);
+					break;
+				}
+				case data::SET_COUNT: {
+					emit_entry_function_set_count(out);
+					break;
+				}
+			}
+		}
+	}
+
 	void StatepredKernel::generate_statepred_filter(std::ostream& out) {
+		out << "__device__ void statepred_filter(u64 state, u32 data[], u64* result_array, u32* result_count) {\n";
+		out << "i32 calculated_count = 0; // don't know if it will satisfy constraint requirements\n";
+		emit_state_prediction_entry_handler(out); // initializes calculated_count, processes entry functions
+
 		CAST_CHILD(pool, data::LootPool, entry->parent);
 		int pool_off = this->pool_memory_offsets[pool->child_index];
 		materialize_level(pool);
 		std::string item_ident = std::to_string(target_constraint.item);
 		std::string arrayPlusIndex = var_name_map[item_ident];
-		
-		
-		out << "__device__ void statepred_filter(u64 state, u32 data[], u64* result_array, u32* result_count) {\n";
-
-
 
 		out << "i32 local_constraints[" << (pool->constraints.size() + 1) << "] = {0};\n";
 		out << arrayPlusIndex << " = calculated_count;\n";

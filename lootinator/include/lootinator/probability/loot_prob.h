b387c60530aeb1bbc90fa977c744a196aaecf26d
ScriptLine <scriptlinestudios@protonmail.com>
2026-03-29 21:46:10 +0200

    api for getting probability table

--- lootinator/include/lootinator/probability/loot_prob.h
+++ lootinator/include/lootinator/probability/loot_prob.h
@@ -6,30 +6,32 @@
 
 namespace prob {
 	struct LootEntry {
-        int item_id;
-        double weight;
-        int count_min;
-        int count_max;
-    };
+		int item_id;
+		double weight;
+		int count_min;
+		int count_max;
+	};
 
-    struct LootPool {
-        int rolls_min;
-        int rolls_max;
-        std::vector<LootEntry> entries;
-    };
+	struct LootPool {
+		int rolls_min;
+		int rolls_max;
+		std::vector<LootEntry> entries;
+	};
 
-    struct LootTable {
-        std::vector<LootPool> pools;
-        LootTable get_for(const data::LootTableRoot& root);
-    };
+	struct LootTable {
+		std::vector<LootPool> pools;
+		LootTable get_for(const data::LootTableRoot& root);
+	};
 
-    struct TargetItem {
-        int item_id;
-        int item_count;
-        bool need_exact_count;
-    };
+	struct TargetItem {
+		int item_id;
+		int item_count;
+		bool need_exact_count;
+	};
 
-    double get_loot_probability(const LootTable& loot_table, const std::vector<prob::TargetItem>& target_items);
-}
+	LootTable get_probability_table(const data::LootTable& loot_table);
+	double get_loot_probability(
+		const LootTable& loot_table, const std::vector<prob::TargetItem>& target_items);
+} // namespace prob
 
 #endif

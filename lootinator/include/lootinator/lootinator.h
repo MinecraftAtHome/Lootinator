7b68c78097efa31cdb50662254fee9bc7d3fcd7e
ScriptLine <scriptlinestudios@protonmail.com>
2026-03-29 21:38:30 +0200

    api sketch

--- lootinator/include/lootinator/lootinator.h
+++ lootinator/include/lootinator/lootinator.h
@@ -5,10 +5,29 @@
 #include <vector>
 
 namespace loot {
-	// std::vector<kgen::ConfiguredKernel>
-	// get_kernels_from_constraints(std::vector<loot::Constraint> &constraints);
-	// std::vector<kgen::ConfiguredKernel> get_kernels_from_constraint_file(std::string
-	// constraint_filepath, std::string loot_table_filepath);
-}
+	/*
+		error codes:
+			0: success
+			1: user constraints too weak
+			2: user constraints not possible
+			3: bad constraint file
+			4: bad loot table
+	*/
+
+	enum LootinatorError {
+		SUCCESS,
+		USER_CONSTRAINT_TOO_WEAK,
+		USER_CONSTRAINT_NOT_POSSIBLE,
+		BAD_CONSTRAINT_FILE,
+		BAD_LOOT_TABLE,
+		INTERNAL_ERROR
+	};
+
+	LootinatorError generate_best_pipeline(const std::string loot_table_filepath,
+		const std::string constraint_filepath, bool use_heuristics, std::string* result);
+	LootinatorError generate_benchmark_source(const std::string loot_table_filepath,
+		const std::string constraint_filepath, std::string* result);
+	std::string parse_errno(LootinatorError error);
+} // namespace loot
 
 #endif
